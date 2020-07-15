361f75e504d3716352cecd7f22d7212ef7131747
ocalvario <oswald.calvario@gmail.com>
2020-07-15 01:56:00 -0400

    Adds initial methods to battle controller

--- app/controllers/battle_controller.rb
+++ app/controllers/battle_controller.rb
@@ -1,2 +1,36 @@
-class BattleController < ApplicationController
+class API::V1::BattleController < ApplicationController
+    def index
+        @battles = Battle.all
+
+        render json: @battles, status: 200
+    end
+
+    def show
+        @battle = Battle.find(params[:id])
+        render json: @battle, status: 200
+    end
+    
+    def create
+        @battle = Battle.create(battle_params)
+
+        render json: @battle, status:200
+    end
+
+    def update
+        @battle = Battle.find(params[:id])
+        @battle.update(battle_params)
+        render json: @battle, status:200
+    end
+
+    def destroy
+        @battle = Battle.find(params[:id])
+        battle.delete
+        render json: {battleID: @battle.id}
+    end
+
+    private
+        def battle_params
+            params.require(:battle).permit(:year)
+        end
+
 end
